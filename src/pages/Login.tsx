feb9b4379655b5f4907ae8f2aa4d7a82c4465527
hugoshi <hugo.amura@contractors.scale.com>
2023-06-20 19:37:11 -0300

    Login ok

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,9 +2,6 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 import { Box, Button, Typography } from '@mui/material'
-import logo from '../assets/images/logo.png';
-import bg_dark from '../assets/images/bg_dark.png';
-import bg_light from '../assets/images/bg_light.png';
 
 import GoogleIcon from '@mui/icons-material/Google';
 
@@ -18,24 +15,14 @@ export default function Login() {
         }
     }, [navigate, user])
     return (
-        <Box sx={{ display: 'flex', width:'100%', height:'100vh'}}>
-            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems: 'center',
-        backgroundImage: `url(${bg_dark})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center', }}>
-                <img src={logo} width='400px' alt='DH chat' />
+        <Box sx={{ display: 'flex', width:'100%', height:'100vh' }}>
+            <Box sx={{ width: '50%' , display:'flex',justifyContent:'center', alignItems:'center', backgroundColor:'#A8CF45'}}>
+                <Typography color={'white'} variant='h3'>DH chat</Typography>
             </Box>
-            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems:'center', 
-        backgroundImage: `url(${bg_light})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center', }}>
-            <Box sx={{ position: 'absolute', marginTop: '250px' }}>
-                <Button variant="contained" onClick={googleSignIn} endIcon={<GoogleIcon />}>
-                    Login whit Google
+            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+                <Button variant="contained" size='large' onClick={googleSignIn} endIcon={<GoogleIcon />}>
+                    Login with Google
                 </Button>
-                </Box>
             </Box>
         </Box>
     )
