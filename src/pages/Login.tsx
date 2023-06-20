a062eaaddea79c3a35f6fc0553b0c7e84137f2cb
hugoshi <hugo.amura@contractors.scale.com>
2023-06-20 19:33:42 -0300

    Login refresh

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,9 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
 import { Box, Button, Typography } from '@mui/material'
+import logo from '../assets/images/logo.png';
+import bg_dark from '../assets/images/bg_dark.png';
+import bg_light from '../assets/images/bg_light.png';
 
 import GoogleIcon from '@mui/icons-material/Google';
 
@@ -13,16 +16,26 @@ export default function Login() {
         if (user) {
             navigate('/')
         }
-    }, [user])
+    }, [navigate, user])
     return (
-        <Box sx={{ display: 'flex', width:'100%', height:'100vh' }}>
-            <Box sx={{ width: '50%' , display:'flex',justifyContent:'center', alignItems:'center', backgroundColor:'#A8CF45'}}>
-                <Typography color={'white'} variant='h3'>DH chat</Typography>
+        <Box sx={{ display: 'flex', width:'100%', height:'100vh'}}>
+            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems: 'center',
+        backgroundImage: `url(${bg_dark})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center', }}>
+                <img src={logo} width='400px' alt='DH chat' />
             </Box>
-            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems:'center', 
+        backgroundImage: `url(${bg_light})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center', }}>
+            <Box sx={{ position: 'absolute', marginTop: '250px' }}>
                 <Button variant="contained" onClick={googleSignIn} endIcon={<GoogleIcon />}>
                     Login whit Google
                 </Button>
+                </Box>
             </Box>
         </Box>
     )
