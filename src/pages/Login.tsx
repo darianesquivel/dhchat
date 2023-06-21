1760981c44a450bfa66e34fa48aaae06a90b7a5f
hugoshi <hugo.amura@contractors.scale.com>
2023-06-21 11:53:47 -0300

    Login new

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,6 @@ import { Box, Button, Typography } from '@mui/material'
 import logo from '../assets/images/logo.png';
 import bg_dark from '../assets/images/bg_dark.png';
 import bg_light from '../assets/images/bg_light.png';
-import hola_hands from '../assets/images/hola_hands.png';
 
 
 import GoogleIcon from '@mui/icons-material/Google';
@@ -20,27 +19,15 @@ export default function Login() {
         }
     }, [navigate, user])
     return (
-        <Box sx={{ display: 'flex', width:'100%', height:'100vh'}}>
-            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems: 'center',
-        backgroundImage: `url(${bg_dark})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center', }}>
+        <Box sx={{ display: 'flex', height:'100vh', justifyContent:'center', alignItems: 'center',
+        backgroundImage: `url(${bg_dark})`, backgroundRepeat: 'repeat' }}>
+            <Box sx={{ position: 'absolute', alignItems: 'center', marginTop: '-100px'  }}>
                 <img src={logo} width='400px' alt='DH chat' />
             </Box>
-            <Box sx={{ width: '50%', display:'flex', justifyContent:'center', alignItems:'center', 
-        backgroundImage: `url(${bg_light})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center', }}>
-            <Box sx={{ position: 'absolute' }}>
-            <img src={hola_hands} width='500px' alt='DH chat' />
-            </Box>
-            <Box sx={{ position: 'absolute', marginTop: '250px' }}>
+            <Box sx={{ position: 'absolute', marginTop: '300px', alignItems: 'center' }}>
                 <Button variant="contained" size='large' onClick={googleSignIn} endIcon={<GoogleIcon />}>
                     Login with Google
                 </Button>
-                </Box>
             </Box>
         </Box>
     )
