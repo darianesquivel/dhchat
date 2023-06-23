2b26580972312521e6b8312bc6d7f0f6579fac1f
hugoshi <hugo.amura@contractors.scale.com>
2023-06-23 11:52:38 -0300

    icons

--- src/pages/CustomDrawer.tsx
+++ src/pages/CustomDrawer.tsx
@@ -17,8 +17,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import GroupsIcon from '@mui/icons-material/Groups';
