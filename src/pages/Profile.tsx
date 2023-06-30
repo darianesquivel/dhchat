96b5b58447899617745deed306e78914bc4f97d0
hugoshi <hugo.amura@contractors.scale.com>
2023-06-30 00:09:57 -0300

    Profile test

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useContext } from 'react';
 import Button from '@mui/material/Button';
 import ListItemText from '@mui/material/ListItemText';
 import ListItem from '@mui/material/ListItem';
@@ -9,15 +10,19 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 
+import { UserAuth } from '../context/AuthContext';
+import { Avatar, Box } from '@mui/material';
+
 
 export default function Profile({onClose}:any) {
 
+  const { user } = UserAuth()
   const handleClose = () => {
     onClose(false);
   };
 
   return (
-    <div>
+    <Box>
         <AppBar sx={{ position: 'relative', backgroundColor: '#A8CF45'}}>
           <Toolbar>
             <IconButton
@@ -29,7 +34,7 @@ export default function Profile({onClose}:any) {
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Sound
+              Profile
             </Typography>
             <Button autoFocus color="inherit" onClick={handleClose}>
               save
@@ -38,16 +43,28 @@ export default function Profile({onClose}:any) {
         </AppBar>
         <List>
           <ListItem>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          <Avatar title={user?.displayName} src={user?.photoURL} 
+  style={{ width: '120px', height: '120px' }} />
           </ListItem>
           <Divider />
           <ListItem>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
+            <ListItemText primary="Nombre" secondary={user?.displayName}  />
           </ListItem>
+          <ListItem>
+            <ListItemText primary="Email" secondary={user?.email}  />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Id" secondary={user?.uid}  />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Country" secondary={'Argentina'}  />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Language" secondary={'Spanish'}  />
+          </ListItem>
+          <Divider />
         </List>
-    </div>
+    </Box>
   );
 }
