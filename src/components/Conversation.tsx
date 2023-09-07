de460f6c4fe36fb85778533f8ee3c9d7abe25a70
Darian <darianesquivelf@gmail.com>
2023-09-07 00:03:33 -0300

    zustand and icons

--- src/components/Conversation.tsx
+++ src/components/Conversation.tsx
@@ -1,77 +1,83 @@
-import { makeStyles } from '@mui/styles'
-import { Avatar, Box, Chip, Typography } from '@mui/material';
-import Message from './Message';
-import useStore from '../context/store';
-import Inputbar from './Inputbar';
+import { makeStyles } from "@mui/styles";
+import { Avatar, Box, Chip, Typography } from "@mui/material";
+import Message from "./Message";
+import useStore from "../context/store";
+import Inputbar from "./Inputbar";
 
 export default function Conversation() {
-    const classes = useStyles()
-    const { messages, currentConversation }: any = useStore()
-    return (
-        <Box className={classes.container}>
-            <Box className={classes.headerConversation}>
-                <Avatar src={currentConversation?.avatar} />
-                <Box className={classes.nameAndParticipants}>
-                    <Typography>{currentConversation?.name}</Typography>
-                    <Box>
-                        {currentConversation?.participants.map((e: any) => <Chip key={e} size='small' label={e} />)}
-                    </Box>
-                </Box>
-            </Box>
-            <Box className={classes.messages}>
-                {messages?.map((message: any, key: any) => {
-                    const { content, avatar, user, translatedContent, sendBy, sendAt, type } =
-                        message;
-                    return (
-                        <Message
-                            key={key}
-                            content={content}
-                            avatar={avatar}
-                            name={user}
-                            translatedContent={translatedContent?.text}
-                            userId={sendBy}
-                            sendAt={sendAt}
-                            lenguage={'en'} // arreglar
-                            translateMe={false} // arreglar
-                            type={type}
-                        />
-                    );
-
-                })}
-            </Box>
-            <Inputbar />
+  const classes = useStyles();
+  const { messages, currentConversation }: any = useStore();
+  return (
+    <Box className={classes.container}>
+      <Box className={classes.headerConversation}>
+        <Avatar src={currentConversation?.avatar} />
+        <Box className={classes.nameAndParticipants}>
+          <Typography variant="overline" fontWeight={800}>
+            {currentConversation?.name}
+          </Typography>
+          <Box>
+            {currentConversation?.participants.map((e: any) => (
+              <Chip key={e} size="small" label={e} />
+            ))}
+          </Box>
         </Box>
-    )
+      </Box>
+      <Box className={classes.messages}>
+        {messages?.map((message: any, key: any) => {
+          const {
+            content,
+            avatar,
+            user,
+            translatedContent,
+            sendBy,
+            sendAt,
+            type,
+          } = message;
+          return (
+            <Message
+              key={key}
+              content={content}
+              avatar={avatar}
+              name={user}
+              translatedContent={translatedContent?.text}
+              userId={sendBy}
+              sendAt={sendAt}
+              lenguage={"en"} // arreglar
+              translateMe={false} // arreglar
+              type={type}
+            />
+          );
+        })}
+      </Box>
+      <Inputbar />
+    </Box>
+  );
 }
 
 const useStyles = makeStyles(() => ({
-    container: {
-        display: 'grid',
-        gridTemplateRows: '60px 1fr 60px',
-        backgroundColor: 'white',
-    },
-    emptyContainer: {
-
-    },
-    headerConversation: {
-        backgroundColor: '#A8CF45',
-        display: 'flex',
-        alignItems: 'center',
-        padding: '8px',
-        gap: 5
-    },
-    nameAndParticipants: {
-
-    },
-    messages: {
-        display: 'flex',
-        flexDirection: 'column',
-        maxHeight: "calc(100vh - 140px)",
-        gap: 10,
-        overflowY: "auto",
-        padding: 8
-    },
-    footerConversation: {
-        backgroundColor: '#A8CF45',
-    }
+  container: {
+    display: "grid",
+    gridTemplateRows: "70px 1fr 70px",
+    backgroundColor: "white",
+  },
+  emptyContainer: {},
+  headerConversation: {
+    backgroundColor: "#A8CF45",
+    display: "flex",
+    alignItems: "center",
+    padding: "8px",
+    gap: 5,
+  },
+  nameAndParticipants: {},
+  messages: {
+    display: "flex",
+    flexDirection: "column",
+    maxHeight: "calc(100vh - 160px)",
+    gap: 5,
+    overflowY: "auto",
+    padding: 8,
+  },
+  footerConversation: {
+    backgroundColor: "#A8CF45",
+  },
 }));
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,145 +1,159 @@
-import { makeStyles } from '@mui/styles'
-import { Avatar, Box, IconButton, Menu, MenuItem } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import React, { useEffect, useState } from 'react';
-import { UserAuth } from '../context/AuthContext';
+import { makeStyles } from "@mui/styles";
 import {
-    collection,
-    query,
-    onSnapshot,
-    orderBy,
-} from "firebase/firestore";
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import React, { useEffect, useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import { db } from "../api/Config/firebase";
-import Chat from './Chat';
-import useStore from '../context/store';
-
+import Chat from "./Chat";
+import useStore from "../context/store";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function Sidebar() {
-    console.log('RENDER DE SIDEBAR')
-    const classes = useStyles()
-    const { setMessages, conversations, setConversations, setCurrentConversation, currentUser }: any = useStore() // Global states
-    const [openMore, setOpenMore] = useState<null | HTMLElement>(null); // State menu 'more' del sidebar header.
+  console.log("RENDER DE SIDEBAR");
+  const classes = useStyles();
+  const {
+    setMessages,
+    conversations,
+    setConversations,
+    setCurrentConversation,
+    currentUser,
+  }: any = useStore(); // Global states
+  const [openMore, setOpenMore] = useState<null | HTMLElement>(null); // State menu 'more' del sidebar header.
 
-    const { logOut, user } = UserAuth() // LogOut y usuario logeado.
+  const { logOut, user } = UserAuth(); // LogOut y usuario logeado.
 
+  const conversationsRef = collection(db, "conversations"); // Ref de conversaciones firestore.
 
-    const conversationsRef = collection(db, "conversations"); // Ref de conversaciones firestore.
-
-    useEffect(() => {
-        const getConversations = onSnapshot(
-            query(conversationsRef, orderBy("createAt", "desc")),
-            (snapshot) => {
-                let conversations: any = [];
-                snapshot.forEach((doc) => {
-                    //trae todas las conversaciones que sean abiertas y las del usuario logeado.
-                    if (doc.data().participants?.includes(user?.email) || doc.data().participants?.includes("open-group")) {
-                        conversations.push({ ...doc.data(), id: doc.id });
-                    }
-                });
-                setConversations(conversations);
-            }
-        );
-        return () => {
-            getConversations();
-
-        };
-        // eslint-disable-next-line
-    }, []);
-
-    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setOpenMore(event.currentTarget); // Abre el menu more, con los 3 puntos.
-    };
-
-    const handleClose = () => {
-        setOpenMore(null); // Cierra el menu 'more'.
-    };
-
-    const handleClickConversation = (conversation: any) => {
-        console.log(conversation)
-        const conversationMessagesRef = collection(
-            db,
-            "conversations",
-            conversation.id,
-            "messages"
-        );
-
-        setCurrentConversation(conversation)
-        const queryMessages = query(conversationMessagesRef, orderBy("sendAt"));
-
-        onSnapshot(queryMessages, (snapshot) => {
-            let messages: any = [];
-            snapshot.forEach((doc) => {
-                messages.push({ ...doc.data(), id: doc.id });
-            });
-            setMessages(messages) // Setea en el estado global los mensajes de la conversacion clickeada para mostrarlos en otro componente.
+  useEffect(() => {
+    const getConversations = onSnapshot(
+      query(conversationsRef, orderBy("createAt", "desc")),
+      (snapshot) => {
+        let conversations: any = [];
+        snapshot.forEach((doc) => {
+          //trae todas las conversaciones que sean abiertas y las del usuario logeado.
+          if (
+            doc.data().participants?.includes(user?.email) ||
+            doc.data().participants?.includes("open-group")
+          ) {
+            conversations.push({ ...doc.data(), id: doc.id });
+          }
         });
-    }
+        setConversations(conversations);
+      }
+    );
+    return () => {
+      getConversations();
+    };
+    // eslint-disable-next-line
+  }, []);
 
-    console.log('aca', currentUser)
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setOpenMore(event.currentTarget); // Abre el menu more, con los 3 puntos.
+  };
 
-    return (
-        <Box className={classes.container}>
-            <Box className={classes.sidebarHeader}>
-                <Avatar src={currentUser?.avatar} />
-                <IconButton onClick={handleMenu}>
-                    <MoreVertIcon />
-                </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={openMore}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(openMore)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={logOut}>Log out</MenuItem>
-                </Menu>
+  const handleClose = () => {
+    setOpenMore(null); // Cierra el menu 'more'.
+  };
+
+  const handleClickConversation = (conversation: any) => {
+    console.log(conversation);
+    const conversationMessagesRef = collection(
+      db,
+      "conversations",
+      conversation.id,
+      "messages"
+    );
+
+    setCurrentConversation(conversation);
+    const queryMessages = query(conversationMessagesRef, orderBy("sendAt"));
+
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages: any = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages); // Setea en el estado global los mensajes de la conversacion clickeada para mostrarlos en otro componente.
+    });
+  };
+
+  return (
+    <Box className={classes.container}>
+      <Box className={classes.sidebarHeader}>
+        <Avatar src={currentUser?.avatar} />
+        <IconButton onClick={handleMenu}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={openMore}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(openMore)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>
+            <ManageAccountsIcon />
+            <Typography>Profile</Typography>
+          </MenuItem>
+          <MenuItem onClick={logOut}>
+            <LogoutIcon />
+            <Typography>Log out</Typography>
+          </MenuItem>
+        </Menu>
+      </Box>
+      <Box className={classes.sidebarContainer}>
+        {conversations?.map((conversation: any, key: any) => {
+          const { lastMessage, avatar, name, id, lastMessageSendBy } =
+            conversation;
+          return (
+            <Box key={key} sx={{ alignItems: "center", margin: 0.5 }}>
+              <Chat
+                lastMessage={lastMessage}
+                lastMessageSendBy={lastMessageSendBy}
+                avatar={avatar}
+                name={name}
+                id={id}
+                onClick={() => handleClickConversation(conversation)}
+              />
             </Box>
-            <Box className={classes.sidebarContainer}>
-                {conversations?.map((conversation: any, key: any) => {
-                    const { lastMessage, avatar, name, id, lastMessageSendBy } =
-                        conversation;
-                    return (
-                        <Box key={key} sx={{ alignItems: 'center', margin: 0.5 }}>
-                            <Chat
-                                lastMessage={lastMessage}
-                                lastMessageSendBy={lastMessageSendBy}
-                                avatar={avatar}
-                                name={name}
-                                id={id}
-                                onClick={() => handleClickConversation(conversation)}
-                            />
-                        </Box>
-                    );
-                })}
-            </Box>
-        </Box>
-    )
+          );
+        })}
+      </Box>
+    </Box>
+  );
 }
 
 const useStyles = makeStyles(() => ({
-    container: {
-        display: 'grid',
-        gridTemplateRows: '60px 1fr',
-        backgroundColor: 'white',
-    },
-    sidebarHeader: {
-        display: 'flex',
-        justifyContent: "space-between",
-        alignItems: 'center',
-        backgroundColor: '#A8CF45',
-        padding: '8px',
-    },
-    sidebarContainer: {
-        maxHeight: "calc(100vh - 80px)",
-        overflowY: "auto",
-    }
+  container: {
+    display: "grid",
+    gridTemplateRows: "70px 1fr",
+    backgroundColor: "white",
+  },
+  sidebarHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#A8CF45",
+    padding: "8px",
+  },
+  sidebarContainer: {
+    maxHeight: "calc(100vh - 80px)",
+    overflowY: "auto",
+  },
 }));
